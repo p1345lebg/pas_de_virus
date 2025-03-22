cb33561669301d7ff523a930b676c2bd1c50e055
p1345lebg <p1345lebg@gmail.com>
2025-03-22 12:36:35 +0100

    ajout de "button" en corrections

---
+++ programmes/components/button.py
@@ -0,0 +1,44 @@
+import pygame
+import os
+
+class Button(pygame.sprite.Sprite):
+    def __init__(self,
+                 screen : pygame.Surface,
+                 position : tuple[tuple[str,str],int,int] = (('ratio','top-left'),0,0), 
+                 size : tuple[int,int] = (10,10), 
+                 action : list = ['none'],
+                 *groups, 
+                 texture : str = 'default/button.png', 
+                 texture_hoover : str = None,
+                 text : str = '',
+                 font : str = 'default/font'):
+        super().__init__(*groups)
+
+        self.screen = screen
+
+        self.size = size
+
+        self.PATH_texture = os.path.join(os.path.dirname(__file__),'..','..','assets',texture)
+        self.texture = pygame.transform.scale(pygame.image.load(self.PATH_texture).convert_alpha(),size)
+        if texture_hoover:
+            self.PATH_texture_hoover = os.path.join(os.path.dirname(__file__),'..','..','assets',texture_hoover)
+            self.texture = pygame.transform.scale(pygame.image.load(self.PATH_texture_hoover).convert_alpha(),size)
+        else:
+            self.texture_hoover = None
+        self.text = font.render(text, True, )
+        
+
+        self.hitbox = self.texture.get_rect()
+        
+
+    def draw(self):
+        cursor_pos = pygame.mouse.get_pos()
+        texture : pygame.Surface = pygame.Surface(self.size)
+
+        if self.texture_hoover and self.hitbox.collidepoint(cursor_pos):
+            texture.blit(self.texture_hoover, (0,0))
+        else:
+            texture.blit(self.texture,(0,0))
+
+        texture.blit(self.text, self.text.get)
+
--- programmes/pasDeVirus/pas_de_virus.py
+++ programmes/pasDeVirus/pas_de_virus.py
@@ -1,4 +1,5 @@
 from .modules import Menu
+from programmes.components import Button
 
 class PasDeVirus:
     def  __init__(self) -> None:
