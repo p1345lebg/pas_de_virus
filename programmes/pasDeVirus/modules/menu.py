a88891cd0dbbd344ec6da2fd3a478ea69b1c2be4
Akirumi <akiirumidesu@gmail.com>
2025-03-26 15:15:15 +0100

    gros caca

--- programmes/pasDeVirus/modules/menu.py
+++ programmes/pasDeVirus/modules/menu.py
@@ -9,12 +9,13 @@ class Menu:
         self.backgroundTileSheet = pygame.image.load(PATH_BACKGROUD_TILESHEET)
         self.background = BackgroundTileSheet(self.screen, PATH_BACKGROUD_TILESHEET)
         self.buttons = {
+            Button(screen,(84,15,'center'),(5,(1,1)),None,os.sep.join(['assets','buttons','cross_pressed.png']),os.sep.join(['assets','buttons','cross_pressed.png']),'',(0,0,0)),
             Button(screen,(50,25,'center'),(20,(20,5)),None,os.sep.join(['assets','buttons','long_button.png']),None,'Select a level',(0,0,0)),
             Button(screen,(25,50,'center'),(5,(5,5)),['pasDeVirus','game','baby','level1'],os.sep.join(['assets','level_icone','easy.png']),None,'',(255,2,10)),
-            Button(screen,(33,60,'center'),(5,(5,5)),['pasDeVirus','game','baby','level1'],os.sep.join(['assets','level_icone','oh.png']),None,'',(255,2,10)),
-            Button(screen,(41,75,'center'),(5,(5,5)),['pasDeVirus','game','baby','level1'],os.sep.join(['assets','level_icone','normal.png']),None,'',(255,2,10)),
-            Button(screen,(75-8-8,75,'center'),(5,(5,5)),['pasDeVirus','game','baby','level1'],os.sep.join(['assets','level_icone','hard.png']),None,'',(255,2,10)),
-            Button(screen,(75-8,60,'center'),(5,(5,5)),['pasDeVirus','game','baby','level1'],os.sep.join(['assets','level_icone','really_hard.png']),None,'',(255,2,10)),
+            Button(screen,(25+25/6,60,'center'),(5,(5,5)),['pasDeVirus','game','baby','level1'],os.sep.join(['assets','level_icone','oh.png']),None,'',(255,2,10)),
+            Button(screen,(25+25/6+25/6,75,'center'),(5,(5,5)),['pasDeVirus','game','baby','level1'],os.sep.join(['assets','level_icone','normal.png']),None,'',(255,2,10)),
+            Button(screen,(75-26/6-26/6,75,'center'),(5,(5,5)),['pasDeVirus','game','baby','level1'],os.sep.join(['assets','level_icone','hard.png']),None,'',(255,2,10)),
+            Button(screen,(75-26/6,60,'center'),(5,(5,5)),['pasDeVirus','game','baby','level1'],os.sep.join(['assets','level_icone','really_hard.png']),None,'',(255,2,10)),
             Button(screen,(75,50,'center'),(5,(5,5)),['pasDeVirus','game','baby','level1'],os.sep.join(['assets','level_icone','diabolical.png']),None,'',(255,2,10))
         }
 
