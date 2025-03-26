7d81f9882713dff3d91104afc01055b5757b1a9f
Akirumi <akiirumidesu@gmail.com>
2025-03-26 11:11:07 +0100

    Menu marche + level icone dans assets

--- programmes/pasDeVirus/modules/menu.py
+++ programmes/pasDeVirus/modules/menu.py
@@ -1,10 +1,16 @@
-from programmes.components import Button
+from programmes.components import Button,BackgroundTileSheet
 import pygame
+import os
 
 class Menu:
     def __init__(self, screen)-> None:
+        self.screen = screen
+        PATH_BACKGROUD_TILESHEET = os.path.join(os.sep.join(['assets','SproutTiles','Tilesets','Grass.png']))
+        self.backgroundTileSheet = pygame.image.load(PATH_BACKGROUD_TILESHEET)
+        self.background = BackgroundTileSheet(self.screen, PATH_BACKGROUD_TILESHEET)
         self.buttons = {
             Button(screen, (0,0,'center'),(10,(2,1)), ['menu','main_menu'])
+            Button(screen,(1,50,"left"),(5,(5,5),['pasDeVirus','game','baby','level1'],os.sep.join(['assets',''])))
         }
 
     def update(self, events):
@@ -14,6 +20,7 @@ class Menu:
                     output = button.handle_click()
                     if output:
                         return output
-        
+        self.background.draw()
+
         for button in self.buttons:
             button.draw()
